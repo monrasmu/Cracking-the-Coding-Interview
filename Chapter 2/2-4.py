24a9cfaaea3fb375cbe8ae3033ead9983e0a9e1f
monrasmu <monrasmu@umich.edu>
2016-12-01 14:13:45 -0500

    Stuck on 2-4

---
+++ Chapter 2/2-4.py
@@ -0,0 +1,4 @@
+# Partition a linked list around a value x, such that all nodes less than
+# x come before all nodes greater than or equal to x
+# EX: input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition = 5]
+#    output: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
